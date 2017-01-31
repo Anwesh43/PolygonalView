bddc2d1fbdc599d9e9d9e7ec5fe1dcc96857f876
Anwesh43 <anweshthecool0@gmail.com>
2017-01-31 13:54:10 +0530

    adding polygonalgraph library

---
+++ app/src/main/java/com/anwesome/ui/polygonalgraphdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.anwesome.ui.polygonalgraphdemo;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anwesome.ui.polygonalgraph.PolygonalGraph;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        PolygonalGraph polygonalGraph = new PolygonalGraph(this,4);
+        addContentView
+    }
+}
