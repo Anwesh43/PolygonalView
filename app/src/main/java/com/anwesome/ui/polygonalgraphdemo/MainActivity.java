05be012aad8884e45257d309070e6c9b8835ede5
Anwesh43 <anweshthecool0@gmail.com>
2017-01-31 13:55:13 +0530

    Rectifiying error

--- app/src/main/java/com/anwesome/ui/polygonalgraphdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/polygonalgraphdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.polygonalgraphdemo;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import com.anwesome.ui.polygonalgraph.PolygonalGraph;
 
@@ -14,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         PolygonalGraph polygonalGraph = new PolygonalGraph(this,4);
-        addContentView
+        addContentView(polygonalGraph,new ViewGroup.LayoutParams(300,300));
     }
 }
