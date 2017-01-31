4e13968c86c16eadfa37af23ef1a7207cb3b21c2
Anwesh43 <anweshthecool0@gmail.com>
2017-01-31 15:13:19 +0530

    using onclicklistener in app

--- app/src/main/java/com/anwesome/ui/polygonalgraphdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/polygonalgraphdemo/MainActivity.java
@@ -3,9 +3,12 @@ package com.anwesome.ui.polygonalgraphdemo;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.anwesome.ui.polygonalgraph.PolygonalGraph;
+import com.anwesome.ui.polygonalgraph.PolygonalGraphHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        PolygonalGraph polygonalGraph = new PolygonalGraph(this,4);
-        addContentView(polygonalGraph,new ViewGroup.LayoutParams(300,300));
+        PolygonalGraphHelper.addPolygonalGraph(this, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Test toast",Toast.LENGTH_SHORT).show();
+            }
+        },5);
+        PolygonalGraphHelper.addPolygonalGraph(this, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Test toast",Toast.LENGTH_SHORT).show();
+            }
+        }, 6, 300, 500);
     }
 }
